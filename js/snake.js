3a3c6f181efc824dc10f54e2b11d228212c6fca7
Sebastian Monzon <smonzon360@gmail.com>
2016-06-17 19:12:00 -0400

    Easter egg

--- js/snake.js
+++ js/snake.js
@@ -10,6 +10,7 @@ var snakeSpeed = 75,
     growthIncr = 5;
 
 changeMode("regular")
+
 function changeSnakeColor(color) {
     if (color == 'yellow') {
         snake_color = " snake-yellow-alive";
@@ -1009,6 +1010,9 @@ SNAKE.Board = SNAKE.Board || (function() {
         me.foodEaten = function() {
             elmLengthPanel.innerHTML = "Length: " + mySnake.snakeLength;
             myFood.randomlyPlaceFood();
+            if (mySnake.snakeLength > 800) {
+                document.getElementById("playingField").style.backgroundImage = "url('http://vignette2.wikia.nocookie.net/someordinarygamers/images/c/cc/Spongebob-funny-pictures.jpg/revision/latest?cb=20140520211423')"
+            }
         };
         
         /**
